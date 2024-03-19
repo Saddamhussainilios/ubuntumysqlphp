8390a3eb24828302332942728be2469b75ed6d92
Saddamhussainilios <158158151+Saddamhussainilios@users.noreply.github.com>
2024-03-19 13:26:01 +0530

    Update index.php- V3

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 
-echo '<h1>This is coming from code pipeline</h>';
-
-
-echo '<h2> changes in code...</h2>';
+echo '<h1>This is coming from code pipeline</h>'
+echo '<h2> changes in code...</h2>'
